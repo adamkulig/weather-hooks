6619935a754cc9329af3064aa7a44c802074c684
Adam Kulig <Adam.Kulig@rossmann.com.pl>
2019-06-17 13:50:15 +0200

    fetching weather hook

--- src/hooks/useInterval.js
+++ src/hooks/useInterval.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 const useInterval = (callback, delay = null) => {
   // const [count, setCount] = useState(0)
