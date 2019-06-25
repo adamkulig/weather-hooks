24a73aca8c1ef0e2fe06a52e8a68d2f41307e36f
Adam Kulig <Adam.Kulig@rossmann.com.pl>
2019-06-25 13:08:07 +0200

    in progress

--- src/features/SearchForm.js
+++ src/features/SearchForm.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Form, Input } from 'reactstrap';
+import { Form, Input, FormGroup, FormFeedback } from 'reactstrap';
 import { MdSearch } from 'react-icons/md';
-import { isEmpty } from 'lodash';
+import { get, isNil } from 'lodash';
 import useFetchData from '../hooks/useFetchData';
+import useFormOperations from '../hooks/useFormOperations';
 
 const INITIAL_VALUES = {
   cityName: ''
@@ -18,25 +19,33 @@ const validate = values => {
 }
 
 const SearchForm = () => {
-  const [searchValue, setSearchValue] = useState('');
+  // const [searchValue, setSearchValue] = useState('');
   const { fetchNewCity } = useFetchData();
+  const { handleChange, handleSubmit, handleFocus, handleBlur, values, errors, isPristine, isSubmitting } = useFormOperations(INITIAL_VALUES, validate)
 
-  const onFormSubmit = async () => {
-    if (!isEmpty(searchValue)) {
-      await fetchNewCity(searchValue)
-      setSearchValue('')
-    }
-  }
+  // const onFormSubmit = async () => {
+  //   if (!isEmpty(searchValue)) {
+  //     await fetchNewCity(searchValue)
+  //     setSearchValue('')
+  //   }
+  // }
+  const isError = !isPristine.cityName && !isNil(errors.cityName)  && errors.cityName.length > 0
 
   return ( 
     <Form className='d-flex justify-content-center'>
-      <Input
-        type='text'
-        name='cityName'
-        value={searchValue}
-        onChange={e => setSearchValue(e.target.value)}
-      />
-      <div className='c-pointer px-1' onClick={onFormSubmit}><MdSearch size={32}/></div>
+      <FormGroup>
+        <Input
+          type='text'
+          name='cityName'
+          value={values.cityName}
+          onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          invalid={isError}
+        />
+        {isError && <FormFeedback>{errors.cityName}</FormFeedback>}
+      </FormGroup>
+      <div className='c-pointer px-1' onClick={handleSubmit}><MdSearch size={32}/></div>
     </Form> 
   );
 }
--- src/hooks/useFormOperations.js
+++ src/hooks/useFormOperations.js
@@ -1,11 +1,58 @@
 import { useEffect, useState } from 'react';
-// import { isString, isArray, isNumber, get } from 'lodash';
+import { map } from 'lodash';
 
-const useFormOperations = (initialValues, validate) => {
+const useFormOperations = (initialValues = {}, validate, submitCallback) => {
+  let initialPristineState = {};
+  map(initialValues, (value, key) => {
+    initialPristineState[key] = true
+  })
+
   const [values, setValues] = useState(initialValues);
   const [errors, setErrors] = useState({});
   const [isSubmitting, setSubmitting] = useState(false);
-  const [isPristine, setPristine] = useState(true);
+  const [isPristine, setPristine] = useState(initialPristineState);
+
+  const handleChange = e => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleFocus = e => {
+    setPristine({
+      ...isPristine,
+      [e.target.name]: false
+    })
+  } 
+
+  const handleBlur = e => {
+    setErrors(validate(values))
+  } 
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setErrors(validate(values))
+    setSubmitting(true)
+  }
+
+  useEffect(() => {
+    // console.log('errors', errors)
+    if(isSubmitting) {
+      const noErrors = Object.keys(errors).length === 0;
+      if (noErrors) {
+        console.log('submit', values)
+        setSubmitting(false)
+      } else {
+        setSubmitting(false)
+      } 
+    }
+  }, [errors])
+  // console.log('values', values)
+  // console.log('errors', errors)
+  // console.log('isPristine', isPristine)
+  console.log('isSubmitting', isSubmitting)
+  return { handleChange, handleSubmit, handleFocus, handleBlur, values, errors, isPristine, isSubmitting }
 }
 
 export default useFormOperations;
